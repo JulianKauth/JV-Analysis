93a6dea120c4920a331d0d51966d1f394344f572
Julian Kauth <julian.kauth@fau.de>
2021-06-22 20:52:31 +0200

    unified plot style and added saving of the plots

--- plot.py
+++ plot.py
@@ -1,8 +1,13 @@
 from matplotlib import pyplot as plt
 from main import *
 
-args = {"linewidth": 2.5, "linestyle": "dotted"}
-args_corrected = {"linewidth": 2.5, "linestyle": "solid"}
+alpha = {"framealpha": 0.85}
+dotted = {"linewidth": 2.5, "linestyle": "dotted"}
+solid = {"linewidth": 2.5, "linestyle": "solid"}
+color_bottom_cell = {"color": "red"}  # "#a02020"
+color_bottom_cell_filtered = {"color": "limegreen"}
+color_top_cell = {"color": "blue"}  # "#0020a0"
+color_combined = {"color": "black"}  # "#010101"
 
 
 def beautify_axis(ax, shift):
@@ -18,22 +23,38 @@ def plot_eqe():
     """Plot the EQE Spectra"""
     plt.xlim(280, 900)
     plt.ylim(0, 0.65)
+    plt.title("EQE Spectra")
     plt.xlabel(r"Wavelength [$nm$]")
     plt.ylabel(r"EQE [%]")
-    plt.plot(eqe_bottom_cell.x, eqe_bottom_cell.y, label="EQE Bottom Cell")
-    plt.plot(eqe_bottom_cell_filtered.x, eqe_bottom_cell_filtered.y, label="EQE Bottom Cell (filtered)")
-    plt.plot(eqe_top_cell.x, eqe_top_cell.y, label="EQE Top Cell")
-    plt.legend()
+    plt.plot(eqe_bottom_cell.x, eqe_bottom_cell.y, label="EQE Bottom Cell", **color_bottom_cell_filtered, **dotted)
+    plt.plot(eqe_bottom_cell_filtered.x, eqe_bottom_cell_filtered.y, label="EQE Bottom Cell (filtered)", **color_bottom_cell, **dotted)
+    plt.plot(eqe_top_cell.x, eqe_top_cell.y, label="EQE Top Cell", **color_top_cell, **dotted)
+    plt.legend(**alpha)
 
+    plt.savefig('plot_eqe.png', bbox_inches='tight')
     plt.show()
     plt.close("all")
 
 
 def plot_solar_spectrum():
+    plt.xlim(280, 900)
+    plt.ylim(0, 1.75)
+    plt.title("Given Solar Spectrum")
+    plt.xlabel(r"Wavelength [$nm$]")
+    plt.ylabel(r"Incident Power [$\frac{W}{nm \cdot m^2}$]")
+    plt.plot(solar_spectrum.x, solar_spectrum.y, **solid)
+
+    plt.savefig('plot_solar_spectrum.png', bbox_inches='tight')
+    plt.show()
+    plt.close("all")
+
+
+def plot_solar_spectra():
     """Plot the solar spectrum"""
 
     fig, host = plt.subplots()
     host.set_xlim(280, 900)
+    plt.title("Solar Spectrum and Resulting Current")
     plt.xlabel(r"Wavelength [$nm$]")
 
     sun = host.twinx()
@@ -41,25 +62,26 @@ def plot_solar_spectrum():
     sun.set_ylabel(r"Sun Spectrum [$\frac{W}{nm \cdot m^2}$]")
     sun.yaxis.label.set_color("red")
     sun.set_ylim(0, 1.75)
-    sun, = sun.plot(solar_spectrum.x, solar_spectrum.y, color="red", label="Solar Spectrum")
+    sun, = sun.plot(solar_spectrum.x, solar_spectrum.y, label="Solar Spectrum", **color_bottom_cell, **solid)
 
     sun_converted = host.twinx()
     beautify_axis(sun_converted, 1.1)
     sun_converted.set_ylabel(r"Sun Spectrum Converted [$\frac{A}{nm \cdot m^2}$]")
     sun_converted.yaxis.label.set_color("green")
     sun_converted.set_ylim(0, 1.3)
-    sun_converted, = sun_converted.plot(solar_spectrum_converted.x, solar_spectrum_converted.y, color="green", label="Solar Spectrum Converted")
+    sun_converted, = sun_converted.plot(solar_spectrum_converted.x, solar_spectrum_converted.y, label="Solar Spectrum Converted", **color_bottom_cell_filtered, **solid)
 
     current = host.twinx()
     beautify_axis(current, 1.2)
     current.set_ylabel(r"Bottom Cell Current [$\frac{mA}{nm \cdot cm^2}$]")
     current.yaxis.label.set_color("blue")
     current.set_ylim(0, 0.04)
-    current, = current.plot(bottom_cell_current.x, bottom_cell_current.y, color="blue", label="Bottom Cell Current")
+    current, = current.plot(bottom_cell_current.x, bottom_cell_current.y, label="Bottom Cell Current", **color_top_cell, **solid)
 
     lines = [sun, sun_converted, current]
     plt.legend(lines, [line.get_label() for line in lines], framealpha=0.85)
     fig.tight_layout()
+    plt.savefig('plot_solar_spectra.png', bbox_inches='tight')
     plt.show()
     plt.close("all")
 
@@ -68,15 +90,17 @@ def plot_currents():
     """plot the current distribution of the cells over wavelength"""
     plt.xlim(280, 900)
     plt.ylim(0, 0.045)
+    plt.title("Current Spectra")
     plt.xlabel(r"Wavelength [$nm$]")
     plt.ylabel(r"Current [$\frac{mA}{nm \cdot cm^2}$]")
-    plt.plot(*bottom_cell_current.plot_data, label="Bottom Cell")
-    plt.plot(*bottom_cell_filtered_current.plot_data, label="Bottom Cell (filtered)")
-    plt.plot(*bottom_cell_current_scaled.plot_data, label="Bottom Cell (filtered, corrected)")
-    plt.plot(*top_cell_current.plot_data, label="Top Cell")
-    plt.plot(*top_cell_current_scaled.plot_data, label="Top Cell (corrected)")
-    plt.legend()
+    plt.plot(*bottom_cell_current.plot_data, label="Bottom Cell", **color_bottom_cell_filtered, **dotted)
+    plt.plot(*bottom_cell_filtered_current.plot_data, label="Bottom Cell (filtered)", **color_bottom_cell, **dotted)
+    plt.plot(*bottom_cell_current_scaled.plot_data, label="Bottom Cell (filtered, corrected)", **color_bottom_cell, **solid)
+    plt.plot(*top_cell_current.plot_data, label="Top Cell", **color_top_cell, **dotted)
+    plt.plot(*top_cell_current_scaled.plot_data, label="Top Cell (corrected)", **color_top_cell, **solid)
+    plt.legend(**alpha)
 
+    plt.savefig('plot_currents.png', bbox_inches='tight')
     plt.show()
     plt.close("all")
 
@@ -85,13 +109,34 @@ def plot_thickness_adjusted_current():
     """plot the current distribution of the cells over wavelength"""
     plt.xlim(280, 900)
     plt.ylim(0, 0.02)
+    plt.title("Current Spectra of Adjusted Cells")
     plt.xlabel(r"Wavelength [$nm$]")
     plt.ylabel(r"Current [$\frac{mA}{nm \cdot cm^2}$]")
-    # plt.plot(*bottom_cell_current.plot_data, label="Bottom Cell")
-    plt.plot(*bottom_cell_current_scaled.plot_data, label="Bottom Cell (filtered)")
-    plt.plot(*top_cell_current_scaled.plot_data, label="Top Cell")
-    plt.legend()
+    plt.plot(*bottom_cell_current_scaled.plot_data, label="Bottom Cell (filtered, corrected)", **color_bottom_cell, **solid)
+    plt.plot(*top_cell_current_scaled.plot_data, label="Top Cell (corrected)", **color_top_cell, **solid)
+    plt.legend(**alpha)
 
+    plt.savefig('plot_currents_adjusted.png', bbox_inches='tight')
+    plt.show()
+    plt.close("all")
+
+
+def plot_jv_given():
+    fig, ax = plt.subplots()
+    ax.grid(True, which='both')
+    ax.axhline(y=0, color='k')
+    ax.axvline(x=0, color='k')
+    plt.title("Given JV-Curves")
+    plt.xlabel(r"Voltage [$V$]")
+    plt.ylabel(r"Current [$\frac{mA}{cm^2}$]")
+
+    plt.ylim(-15, 5)
+    plt.plot(*jv_bottom_cell.plot_data, label="Bottom Cell", **color_bottom_cell_filtered, **dotted)
+    plt.plot(*jv_bottom_cell_filtered.plot_data, label="Bottom Cell (filtered)", **color_bottom_cell, **dotted)
+    plt.plot(*jv_top_cell.plot_data, label="Top Cell", **color_top_cell, **dotted)
+    plt.legend(**alpha)
+
+    plt.savefig('plot_jv_given.png', bbox_inches='tight')
     plt.show()
     plt.close("all")
 
@@ -101,18 +146,19 @@ def plot_jv_separate():
     ax.grid(True, which='both')
     ax.axhline(y=0, color='k')
     ax.axvline(x=0, color='k')
+    plt.title("JV-Curves, Given and Adjusted")
     plt.xlabel(r"Voltage [$V$]")
     plt.ylabel(r"Current [$\frac{mA}{cm^2}$]")
 
     plt.ylim(-15, 5)
-    plt.title("JV-Curves")
-    plt.plot(*jv_bottom_cell.plot_data, label="Bottom Cell")
-    plt.plot(*jv_bottom_cell_filtered.plot_data, label="Bottom Cell (filtered)")
-    plt.plot(*jv_bottom_cell_corrected.plot_data, label="Bottom Cell (filtered, corrected)")
-    plt.plot(*jv_top_cell.plot_data, label="Top Cell")
-    plt.plot(*jv_top_cell_corrected.plot_data, label="Top Cell (corrected)")
-    plt.legend()
+    plt.plot(*jv_bottom_cell.plot_data, label="Bottom Cell", **color_bottom_cell_filtered, **dotted)
+    plt.plot(*jv_bottom_cell_filtered.plot_data, label="Bottom Cell (filtered)", **color_bottom_cell, **dotted)
+    plt.plot(*jv_bottom_cell_corrected.plot_data, label="Bottom Cell (filtered, corrected)", **color_bottom_cell, **solid)
+    plt.plot(*jv_top_cell.plot_data, label="Top Cell", **color_top_cell, **dotted)
+    plt.plot(*jv_top_cell_corrected.plot_data, label="Top Cell (corrected)", **color_top_cell, **solid)
+    plt.legend(**alpha)
 
+    plt.savefig('plot_jv.png', bbox_inches='tight')
     plt.show()
     plt.close("all")
 
@@ -122,19 +168,20 @@ def plot_jv_combined():
     ax.grid(True, which='both')
     ax.axhline(y=0, color='k')
     ax.axvline(x=0, color='k')
+    plt.title("JV-Curves of Integrated Cells")
     plt.xlabel(r"Voltage [$V$]")
     plt.ylabel(r"Current [$\frac{mA}{cm^2}$]")
 
     plt.ylim(-15, 5)
-    plt.title("JV-Curves combined")
-    plt.plot(*jv_combined.plot_data, **args, label="Combined", color="#010101")
-    plt.plot(*jv_combined_corrected.plot_data, **args_corrected, label="Combined (corrected)", color="#010101")
-    plt.plot(*jv_bottom_cell_filtered.plot_data, **args, label="Bottom Cell (filtered)", color="#a02020")
-    plt.plot(*jv_bottom_cell_corrected.plot_data, **args_corrected, label="Bottom Cell (filtered, corrected)", color="#a02020")
-    plt.plot(*jv_top_cell.plot_data, **args, label="Top Cell", color="#0020a0")
-    plt.plot(*jv_top_cell_corrected.plot_data, **args_corrected, label="Top Cell (corrected)", color="#0020a0")
-    plt.legend()
+    plt.plot(*jv_combined.plot_data, label="Combined", **color_combined, **dotted)
+    plt.plot(*jv_combined_corrected.plot_data, label="Combined (corrected)", **color_combined, **solid)
+    plt.plot(*jv_bottom_cell_filtered.plot_data, label="Bottom Cell (filtered)", **color_bottom_cell, **dotted)
+    plt.plot(*jv_bottom_cell_corrected.plot_data, label="Bottom Cell (filtered, corrected)", **color_bottom_cell, **solid)
+    plt.plot(*jv_top_cell.plot_data, label="Top Cell", **color_top_cell, **dotted)
+    plt.plot(*jv_top_cell_corrected.plot_data, label="Top Cell (corrected)", **color_top_cell, **solid)
+    plt.legend(**alpha)
 
+    plt.savefig('plot_jv_combined.png', bbox_inches='tight')
     plt.show()
     plt.close("all")
 
@@ -144,6 +191,7 @@ def plot_jv_power():
     ax.grid(True, which='both')
     ax.axhline(y=0, color='k')
     ax.axvline(x=0, color='k')
+    plt.title("JV- and Power-Curves of Tandem Cells")
     ax.set_xlabel(r"Voltage [$V$]")
     ax.set_ylabel(r"Current [$\frac{mA}{cm^2}$]")
     ax.set_xlim(-0.5, 1.6)
@@ -151,26 +199,29 @@ def plot_jv_power():
     power = ax.twinx()
     power.set_ylabel(r"Power [$\frac{mW}{cm^2}$]")
     power.set_ylim(-5, 3)
-    a, = power.plot(*power_combined.plot_data, **args, color="red", label="Combined Power")
-    b, = power.plot(*power_combined_corrected.plot_data, **args_corrected, color="red", label="Combined Power (corrected)")
+    a, = power.plot(*power_combined.plot_data, **dotted, **color_bottom_cell, label="Combined Power")
+    b, = power.plot(*power_combined_corrected.plot_data, **solid, **color_bottom_cell, label="Combined Power (corrected)")
 
     ax.set_ylim(-5, 3)
-    c, = ax.plot(*jv_combined.plot_data, **args, label="Combined JV", color="#010101")
-    d, = ax.plot(*jv_combined_corrected.plot_data, **args_corrected, label="Combined JV (corrected)", color="#010101")
+    c, = ax.plot(*jv_combined.plot_data, **dotted, label="Combined JV", **color_combined)
+    d, = ax.plot(*jv_combined_corrected.plot_data, **solid, label="Combined JV (corrected)", **color_combined)
 
     lines = [a, b, c, d]
     plt.legend(lines, [line.get_label() for line in lines], framealpha=0.85, loc="upper center")
     fig.tight_layout()
 
+    plt.savefig('plot_power.png', bbox_inches='tight')
     plt.show()
     plt.close("all")
 
 
 if __name__ == '__main__':
-    # plot_eqe()
-    # plot_solar_spectrum()
-    # plot_currents()
-    # plot_thickness_adjusted_current()
-    # plot_jv_separate()
-    # plot_jv_combined()
+    plot_eqe()
+    plot_solar_spectrum()
+    plot_solar_spectra()
+    plot_currents()
+    plot_thickness_adjusted_current()
+    plot_jv_given()
+    plot_jv_separate()
+    plot_jv_combined()
     plot_jv_power()
